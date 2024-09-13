9b758caf44931f7b2c4f333754414253f9115936
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-13 13:30:48 +0530

    Input & Output

---
+++ Level 1/Input & Output.cs
@@ -0,0 +1,19 @@
+https://www.interviewbit.com/problems/input-output-in-c/
+
+using System;
+using System.IO;
+
+class MAIN  {
+    public static void Main(string[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUT
+        // E.g. 'StreamReader' for input & 'StreamWriter' for output
+        string input;
+        
+        input=Console.ReadLine();
+        
+        Console.WriteLine("Hello " + input);
+        
+    }
+}
