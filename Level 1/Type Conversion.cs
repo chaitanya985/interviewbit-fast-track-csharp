2d8ce8ff7bae8865cc8fc0c0b6a04f55ced9397d
chaitanya985 <chaitanya.panicker98@gmail.com>
2024-09-13 13:28:20 +0530

    Type Conversion

---
+++ Level 1/Type Conversion.cs
@@ -0,0 +1,16 @@
+https://www.interviewbit.com/problems/type-conversion-in-c/
+
+using System;
+using System.IO;
+
+class MAIN  {
+    public static void Main(string[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUT
+        // E.g. 'StreamReader' for input & 'StreamWriter' for output
+        int ch = Console.Read();
+        
+        Console.Write(ch);
+    }
+}
